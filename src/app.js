1caee0fbd6f4998b5eb478d8bc20efdfa6341ccc
Eduardo Pina <pinacps@gmail.com>
2020-08-17 22:49:45 -0300

    inclusao funcoes nos metodos http

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require("uuidv4");
 
 // const { uuid } = require("uuidv4");
 
@@ -11,23 +12,46 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).send(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repositorie = { id: uuid(), title, url, techs, likes: 0 };
+  repositories.push(repositorie);
+  return response.status(200).send(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const index = repositories.findIndex(item => item.id == id);
+  if (index < 0)
+    return response.status(400).json({ message: "Register not found." });
+  const repositorie = repositories[index];
+  repositorie.title = title;
+  repositorie.url = url;
+  repositorie.techs = techs;
+  return response.status(200).send(repositorie);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex(item => item.id == id);
+  if (index < 0)
+    return response.status(400).json({ message: "Register not found." });
+  repositories.splice(index, 1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex(item => item.id == id);
+  if (index < 0)
+    return response.status(400).json({ message: "Register not found." });
+  const repositorie = repositories[index];
+  repositorie.likes++;
+  return response.status(200).send(repositorie);
 });
 
 module.exports = app;
